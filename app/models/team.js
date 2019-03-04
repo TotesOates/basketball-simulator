794a882d55dee27d1385b13d0aee6531602c1582
TotesOates <joseph.chen.od@gmail.com>
2019-03-03 19:41:53 -0600

    fix bug type in team.js

--- app/models/team.js
+++ app/models/team.js
@@ -1,50 +1,20 @@
-import DS from 'ember-data';
-import { computed } from '@ember/object';
-import { union, filterBy, sum, mapBy } from '@ember/object/computed';
+import DS from "ember-data";
+import { union, filterBy} from "@ember/object/computed";
 
 export default DS.Model.extend({
-  name: DS.attr('string'),
-  homeGames: DS.hasMany('game', {inverse: 'homeTeam'}),
-  awayGames: DS.hasMany('game', {inverse: 'awayTeam'}),
-  
-  games: union('homeGames', 'awayGames'),
+  name: DS.attr("string"),
+  homeGames: DS.hasMany("game", { inverse: "homeTeam" }),
+  awayGames: DS.hasMany("game", { inverse: "awayTeam" }),
 
-  homeGamesWon: filterBy('homesGames', 'isHomeWin'),
-  awayGamesWon: filterBy('awayGames', 'isAwayWin'),
-  gamesWon: union('homeGamesWon', 'awayGamesWon'),
+  games: union("homeGames", "awayGames"),
 
-  homeGamesLost: filterBy('homeGames', 'isAwayWin'),
-  awayGamesLost: filterBy('awayGames', 'isHomeWin'),
-  gamesLost: union('homeGamesLost', 'awayGamesLost'),
+  homeGamesWon: filterBy("homeGames", "isHomeWin"),
+  awayGamesWon: filterBy("awayGames", "isAwayWin"),
+  gamesWon: union("homeGamesWon", "awayGamesWon"),
 
-  gamesDrawn: filterBy('games', 'isDraw'),
+  homeGamesLost: filterBy("homeGames", "isAwayWin"),
+  awayGamesLost: filterBy("awayGames", "isHomeWin"),
+  gamesLost: union("homeGamesLost", "awayGamesLost"),
 
-  homeGoalsScoredArray: mapBy('homeGames', 'homeGoals'),
-  homeGoalsScored: sum('homeGoalsScoredArray'),
-
-  awayGoalsScoredArray: mapBy('awayGames', 'awayGoals'),
-  awayGoalsScored: sum('awayGoalsScoredArray'),
-
-  goalsScored: computed('homeGoalsScored', 'awayGoalsScored', function() {
-    return this.homeGoalsScored + this.awayGoalsScored;
-  }),
-
-  homeGoalsConcededArray: mapBy('homeGames', 'awayGoals'),
-  homeGoalsConceded: sum('homeGoalsConcededArray'),
-
-  awayGoalsConcededArray: mapBy('awayGames', 'homeGoals'),
-  awayGoalsConceded: sum('awayGoalsConcededArray'),
-
-  goalsConceded: computed('homeGoalsConceded', 'awayGoalsConceded', function() {
-    return this.homeGoalsConceded + this.awayGoalsConceded;
-  }),
-
-  goalDifference: computed('goalsScored', 'goalsConceded', function() {
-    return this.goalsScored - this.goalsConceded;
-  }),
-
-  points: computed('gamesWon.length', 'gamesDrawn.length', function() {
-    return (this.gamesWon.length * 3) + this.gamesDrawn.length;
-  })
-
+  gamesDrawn: filterBy("games", "isDraw"),
 });
